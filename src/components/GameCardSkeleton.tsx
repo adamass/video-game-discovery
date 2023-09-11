ec508f6c0c0499617553f7da0d257f290954fdd5
adamass <43586138+adamass@users.noreply.github.com>
2023-09-11 10:22:11 +0200

    Ship genres and platforms with app

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 export const GameCardSkeleton = () => {
   return (
     <Card>
-      <Skeleton height="200px" />
+      <Skeleton height="320px" />
       <CardBody>
         <SkeletonText />
       </CardBody>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,10 @@ interface Props {
 
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  const skeletons = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27,
+  ];
 
   return (
     <>
